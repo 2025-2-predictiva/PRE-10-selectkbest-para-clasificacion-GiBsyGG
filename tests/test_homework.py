e5021f45ea69997a1f9889850258dfa21ee11e96
GiBsyGG <jcval2801@gmail.com>
2025-10-04 10:07:23 -0500

    Homework Sent

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -20,9 +20,9 @@ def load_estimator():
     import os
     import pickle
 
-    if not os.path.exists("estimator.pickle"):
+    if not os.path.exists("homework/estimator.pickle"):
         return None
-    with open("estimator.pickle", "rb") as file:
+    with open("homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
     return estimator
